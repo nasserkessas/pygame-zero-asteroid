821ae0c152fc693c97097256383b2e5445e5aced
Nasser <nasser@kessas.com>
2020-10-25 14:46:54 +1000

    added shooting

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ WIDTH = 800
 HEIGHT = 600
 SHIP_SPEED = 4
 
+score = 0
 game_over = False
 game_over_frame = 0
 spaceship = Actor("spaceship")
@@ -14,10 +15,11 @@ KEYS = {
     "right": False,
     "up": False,
     "down": False,
+    "space": False
 }
 
-def make_star(y=None):
-    size = randint(1,3)
+def make_star(y = None):
+    size = randint(1, 3)
     stars.append({"x": randint(0, WIDTH), "y": y if y != None else randint(0, HEIGHT), "width": size, "shade": 127 + size * 32})
 
 def make_asteroid():
@@ -27,9 +29,15 @@ def make_asteroid():
         "speed": uniform(1, 3)
     })
 
+def fill_with_zeros(num):
+    length = len(str(num))
+    zeros = 8 - length
+    return("0" * zeros + str(num))
+
 def start():
-    global game_over, game_over_frame, spaceship
+    global game_over, game_over_frame, spaceship, score, bullets
     STAR_COUNT = 50
+    score = 0
     ASTEROID_COUNT = 5
     game_over = False
     game_over_frame = 0
@@ -40,8 +48,7 @@ def start():
 
     stars = []
     asteroids = []
-
-
+    bullets = []
 
     for i in range(STAR_COUNT):
         make_star()
@@ -55,7 +62,7 @@ def start():
 start()
 
 def draw():
-    global game_over_frame
+    global game_over_frame, score
     screen.clear()
     screen.fill((15, 4, 30))
 
@@ -63,8 +70,12 @@ def draw():
         screen.draw.filled_circle((star["x"], star["y"]), star["width"], (star["shade"], star["shade"], star["shade"]))
     for asteroid in asteroids:
         asteroid["actor"].draw()
+    for bullet in bullets:
+        bullet["actor"].draw()
     if game_over_frame < 4:
         spaceship.draw()
+
+    screen.draw.text("Score: " + fill_with_zeros(score), color = "white", topright=(WIDTH-25,25), fontsize=60)
 
     if game_over:
         if game_over_frame >= 0 and game_over_frame < 1:
@@ -78,10 +89,8 @@ def draw():
             screen.draw.text("Press ENTER to play again", color = "white", center=(WIDTH/2,HEIGHT/2+50), fontsize=35)
         game_over_frame += 0.6
 
-
-
 def update():
-    global game_over
+    global game_over, score
     if game_over_frame < 4:
         for star in stars:
             star["y"] += 0.5
@@ -92,12 +101,12 @@ def update():
         for asteroid in asteroids:
             actor = asteroid["actor"]
             actor.bottom += asteroid["speed"]
-            if actor.bottom > HEIGHT:
+            if actor.top > HEIGHT:
                 asteroids.remove(asteroid)
                 make_asteroid()
                 asteroids[len(asteroids)-1]["actor"].pos = (randint(0, WIDTH), 0)
+                score += 1000
             actor.angle += asteroid["rotation_speed"]
-
 
         if KEYS["left"] and spaceship.left > 0:
             spaceship.left -= SHIP_SPEED
@@ -108,16 +117,30 @@ def update():
         if KEYS["down"] and spaceship.bottom < HEIGHT:
             spaceship.top += SHIP_SPEED
 
-
     for asteroid in asteroids:
         touching = spaceship.colliderect(asteroid["actor"])
         if touching:
             game_over = True
 
+    for bullet in bullets:
+        if bullet["actor"].bottom == 0:
+            bullets.remove(bullet)
+        for asteroid in asteroids:
+            if bullet["actor"].colliderect(asteroid["actor"]):
+                bullets.remove(bullet)
+                asteroids.remove(asteroid)
+                make_asteroid()
+                asteroids[len(asteroids) - 1]["actor"].pos = (randint(0, WIDTH), 0)
+                score += 500
+        bullet["actor"].bottom -= 3
 
+
+def shoot():
+    bullets.append({"actor": Actor("bullet")})
+    bullets[len(bullets)-1]["actor"].pos = (spaceship.center[0], spaceship.top)
 
 def on_key_down(key):
-    global KEYS
+    global KEYS, game_over
     if key == keys.RIGHT:
         KEYS["right"] = True
     if key == keys.LEFT:
@@ -126,6 +149,8 @@ def on_key_down(key):
         KEYS["up"] = True
     if key == keys.DOWN:
         KEYS["down"] = True
+    if key == keys.SPACE and not game_over:
+        shoot()
 
     if game_over_frame > 4 and key == keys.RETURN:
         start()
@@ -140,27 +165,8 @@ def on_key_up(key):
         KEYS["up"] = False
     if key == keys.DOWN:
         KEYS["down"] = False
+    if key == keys.SPACE:
+        KEYS["space"] = False
+
 
 clock.schedule_interval(make_asteroid, 10)
-
-'''
-asteroid = []
-def generate():
-    global asteroid
-    verticies = randint(10, 20)
-    center = {"x": randint(0, WIDTH), "y": randint(0, HEIGHT)}
-    for i in range(verticies):
-        distance = randint(30, 50)
-        deg = randint(1, 360)
-        asteroid.append({"distance": distance, "degrees": deg, "x": center["x"] + distance / sin(deg * pi / 180), "y": center["y"] + distance / cos(deg * pi / 180)})
-    print(str(asteroid), "length: ", str(len(asteroid)))
-
-generate()
-
-def draw():
-    global asteroid
-    screen.clear()
-    screen.fill((21, 8, 54))
-    for i in range(len(asteroid)):
-        screen.draw.line((asteroid[i]["x"], asteroid[i]["y"]), (asteroid[i+1 if (i+1) < len(asteroid) else 0]["x"], asteroid[i+1 if (i+1) < len(asteroid) else 0]["y"]), (100, 100, 100))
-'''
