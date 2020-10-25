3cc31c9a563ac7d1db3cf07c9ab1139545183ed5
Nasser <nasser@kessas.com>
2020-10-25 12:16:07 +1000

    added game over and restart

--- game.py
+++ game.py
@@ -3,12 +3,12 @@ from random import randint, uniform
 WIDTH = 800
 HEIGHT = 600
 
-ASTEROID_COUNT = 5
-STAR_COUNT = 50
 SHIP_SPEED = 4
+game_over = False
+game_over_frame = 0
+spaceship = Actor("spaceship")
+spaceship.pos = (WIDTH / 2, HEIGHT - 55)
 
-left_pressed = False
-right_pressed = False
 KEYS = {
     "left": False,
     "right": False,
@@ -16,69 +16,102 @@ KEYS = {
     "down": False,
 }
 
-spaceship = Actor("spaceship")
-spaceship.pos = (WIDTH / 2, HEIGHT - 55)
-
-stars = []
-asteroids = []
-
 def make_star(y=None):
     size = randint(1,3)
     stars.append({"x": randint(0, WIDTH), "y": y if y != None else randint(0, HEIGHT), "width": size, "shade": 127 + size * 32})
 
+def start():
+    global game_over, game_over_frame, spaceship
+    STAR_COUNT = 50
+    ASTEROID_COUNT = 5
+    game_over = False
+    game_over_frame = 0
 
-for i in range(STAR_COUNT):
-    make_star()
+    spaceship = Actor("spaceship")
+    spaceship.pos = (WIDTH / 2, HEIGHT - 55)
+    global stars, asteroids
 
-for i in range(ASTEROID_COUNT):
-    asteroids.append({
-    "actor": Actor("asteroid_" + str(randint(1, 5))),
-    "rotation_speed": uniform(0.3, 2),
-    "speed": uniform(1, 3)
-    })
+    stars = []
+    asteroids = []
 
-for asteroid in asteroids:
-    asteroid["actor"].pos = (randint(0, WIDTH), 0)
 
+
+    for i in range(STAR_COUNT):
+        make_star()
+
+    for i in range(ASTEROID_COUNT):
+        asteroids.append({
+        "actor": Actor("asteroid_" + str(randint(1, 5))),
+        "rotation_speed": uniform(0.3, 2),
+        "speed": uniform(1, 3)
+        })
+
+    for asteroid in asteroids:
+        asteroid["actor"].pos = (randint(0, WIDTH), 0)
+
+start()
 
 def draw():
+    global game_over_frame
     screen.clear()
     screen.fill((15, 4, 30))
 
     for star in stars:
         screen.draw.filled_circle((star["x"], star["y"]), star["width"], (star["shade"], star["shade"], star["shade"]))
-
     for asteroid in asteroids:
         asteroid["actor"].draw()
-    spaceship.draw()
+    if game_over_frame < 4:
+        spaceship.draw()
+
+    if game_over:
+        if game_over_frame >= 0 and game_over_frame < 1:
+            spaceship.image = "explosion_1"
+        if game_over_frame >= 1 and game_over_frame < 2:
+            spaceship.image = "explosion_2"
+        if game_over_frame >= 2 and game_over_frame < 3:
+            spaceship.image = "explosion_3"
+        if game_over_frame>4:
+            screen.draw.text("GAME OVER", color = "white", center=(WIDTH/2,HEIGHT/2), fontsize=80)
+            screen.draw.text("Press ENTER to play again", color = "white", center=(WIDTH/2,HEIGHT/2+50), fontsize=35)
+        game_over_frame += 0.4
+
 
 
 def update():
-    for star in stars:
-        star["y"] += 0.5
-        if star["y"] > HEIGHT:
-            stars.remove(star)
-            make_star(0)
+    global game_over
+    if game_over_frame < 4:
+        for star in stars:
+            star["y"] += 0.5
+            if star["y"] > HEIGHT:
+                stars.remove(star)
+                make_star(0)
+
+        for asteroid in asteroids:
+            actor = asteroid["actor"]
+            actor.bottom += asteroid["speed"]
+            if actor.bottom > HEIGHT:
+                asteroids.remove(asteroid)
+                asteroids.append({"actor": Actor("asteroid_" + str(randint(1, 5))), "rotation_speed": uniform(0.3, 2), "speed": uniform(1, 3)})
+                asteroids[len(asteroids)-1]["actor"].pos = (randint(0, WIDTH), 0)
+            actor.angle += asteroid["rotation_speed"]
+
+
+        if KEYS["left"] and spaceship.left > 0:
+            spaceship.left -= SHIP_SPEED
+        if KEYS["right"] and spaceship.right < WIDTH:
+            spaceship.left += SHIP_SPEED
+        if KEYS["up"] and spaceship.top > 0:
+            spaceship.top -= SHIP_SPEED
+        if KEYS["down"] and spaceship.bottom < HEIGHT:
+            spaceship.top += SHIP_SPEED
+
 
     for asteroid in asteroids:
-        actor = asteroid["actor"]
-        actor.bottom += asteroid["speed"]
-        if actor.bottom > HEIGHT:
-            asteroids.remove(asteroid)
-            asteroids.append({"actor": Actor("asteroid_" + str(randint(1, 5))), "rotation_speed": uniform(0.3, 2), "speed": uniform(1, 3)})
-            asteroids[len(asteroids)-1]["actor"].pos = (randint(0, WIDTH), 0)
-        actor.angle += asteroid["rotation_speed"]
-    spaceship.angle = 180
+        touching = spaceship.colliderect(asteroid["actor"])
+        if touching:
+            game_over = True
 
 
-    if KEYS["left"] and spaceship.left > 0:
-        spaceship.left -= SHIP_SPEED
-    if KEYS["right"] and spaceship.right < WIDTH:
-        spaceship.left += SHIP_SPEED
-    if KEYS["up"] and spaceship.top > 0:
-        spaceship.top -= SHIP_SPEED
-    if KEYS["down"] and spaceship.bottom < HEIGHT:
-        spaceship.top += SHIP_SPEED
 
 def on_key_down(key):
     global KEYS
@@ -90,6 +123,9 @@ def on_key_down(key):
         KEYS["up"] = True
     if key == keys.DOWN:
         KEYS["down"] = True
+
+    if game_over_frame > 4 and key == keys.RETURN:
+        start()
 
 def on_key_up(key):
     global KEYS
